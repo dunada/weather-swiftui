42f3d73670407acb0ff56f57b60d440b8551b3dc
Roy <eduardo_rodrigues@epam.com>
2024-02-26 19:10:07 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Eduardo Rodrigues on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
